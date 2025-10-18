e472060357a104663ddda9f1ea4b90df62a172b6
gh0st1457 <sami22205101457@diu.edu.bd>
2025-10-18 17:00:50 +0600

    header

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,9 +1,20 @@
-@extends('layouts.main')
-@section('content')
+
 <header>
+    <style>
+        .nav{
+            display: flex;
+            justify-content: space-between;
+            align-items: center;
+            height: 64px;
+            background-color: grey;
+            border: 1px solid black;
+            padding:5px; 
+        }
+        </style>
+
 <nav class='nav'>
     <div class='home'>Home</div>
     <div class='project'>Projects</div>
-    <div class='skills'><a heref='/skills'>Skills</a></div>
+    <div class='skills'><a href='/skills'>Skills</a></div>
 </nav>
 </header>
