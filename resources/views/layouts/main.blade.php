10da5048a0a461b25fcfa087a9bd9a3b9d6f66f2
gh0st1457 <sami22205101457@diu.edu.bd>
2025-10-18 14:27:02 +0600

    structure

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -2,12 +2,12 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>this is portfolio</title>
+   
 </head>
 <body>
-    <h1>Welcome to My Portfolio</h1>
-    <p>This is the welcome page of my portfolio website.</p>
-    
+    @include('partial.header')
+    @yield('content')
 </body>
 </html>
---
+++ resources/views/pages/home.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts.main')
+@section('content')
+<h1>
+    thiss isa laravel project
+</h1>
+@endsection
